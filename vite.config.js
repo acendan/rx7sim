7dd1d0503c9c70f9951339c6bca3f27ef10b656d
Aaron Cendan <aaron.cendan@gmail.com>
2025-10-23 10:25:18 -0700

    Use build.rollupOptions.output.manualChunks to improve chunking

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,15 @@ export default {
     {
         outDir: '../dist',
         emptyOutDir: true,
-        sourcemap: true
+        sourcemap: true,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                }
+            }
+        }
     }
 }
